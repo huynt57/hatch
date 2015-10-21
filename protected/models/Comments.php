3c37a579068d50751a859998a73bf94e0f8c2583
huynt <huynt57@gmail.com>
2015-10-21 16:50:03 +0700

    comment models

--- protected/models/Comments.php
+++ protected/models/Comments.php
@@ -2,14 +2,30 @@
 
 Yii::import('application.models._base.BaseComments');
 
-class Comments extends BaseComments
-{
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
-        
-        public function addComment($attr)
-        {
-            
+class Comments extends BaseComments {
+
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    public function addComment($attr) {
+        $model = new Comments;
+        $model->setAttributes($attr);
+        $model->created_at = time();
+        $model->updated_at = time();
+        if ($model->save(FALSE)) {
+            return TRUE;
         }
+        return FALSE;
+    }
+    
+    public function getCommentsByEvent($event_id, $limit, $offset)
+    {
+        $criteria = new CDbCriteria;
+        $criteria->limit = $limit;
+        $criteria->offset = $offset;
+        $criteria->condition = "event_id = $event_id";
+        $data = Comments::model()->findAll($criteria);
+    }
+
 }
