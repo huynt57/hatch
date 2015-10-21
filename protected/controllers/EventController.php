e3f21eeeb66f04d0e51647163cc98cee6832b929
huynt <huynt57@gmail.com>
2015-10-21 16:06:35 +0700

    add event api

--- protected/controllers/EventController.php
+++ protected/controllers/EventController.php
@@ -1,51 +1,66 @@
 <?php
 
-class EventController extends Controller
-{
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
-        
-        public function actionAddEvent()
-        {
-            
-        }
-        
-        public function actionGetEvent()
-        {
-            
-        }
-        
-        public function actionGetEventByUser()
-        {
-            
-        }
+class EventController extends Controller {
 
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
+    public function actionIndex() {
+        $this->render('index');
+    }
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+    public function actionAddEvent() {
+        try {
+            $attr = StringHelper::filterArrayString($_POST);
+            if (Events::model()->addEvent($attr)) {
+                ResponseHelper::JsonReturnSuccess('', 'Success');
+            } else {
+                ResponseHelper::JsonReturnError('', 'Server Error');
+            }
+        } catch (Exception $ex) {
+            var_dump($ex->getMessage());
+        }
+    }
+
+    public function actionGetEvent() {
+        $request = Yii::app()->request;
+        try {
+            
+        } catch (Exception $ex) {
+            var_dump($ex->getMessage());
+        }
+    }
+
+    public function actionGetEventByUser() {
+        $request = Yii::app()->request;
+        try {
+            
+        } catch (Exception $ex) {
+            var_dump($ex->getMessage());
+        }
+    }
+
+    // Uncomment the following methods and override them if needed
+    /*
+      public function filters()
+      {
+      // return the filter configuration for this controller, e.g.:
+      return array(
+      'inlineFilterName',
+      array(
+      'class'=>'path.to.FilterClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+
+      public function actions()
+      {
+      // return external action classes, e.g.:
+      return array(
+      'action1'=>'path.to.ActionClass',
+      'action2'=>array(
+      'class'=>'path.to.AnotherActionClass',
+      'propertyName'=>'propertyValue',
+      ),
+      );
+      }
+     */
 }
